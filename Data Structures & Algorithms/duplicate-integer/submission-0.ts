a1521f1fe80bf0f8dbb24b5069b0e02490994fdc
Thiago <thiago6891@gmail.com>
2026-05-18 07:32:12 -0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.ts
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        const set = new Set();
+        for (const n of nums) {
+            if (set.has(n)) return true;
+            set.add(n);
+        }
+        return false;
+    }
+}
