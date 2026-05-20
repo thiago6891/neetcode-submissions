475444b3895199c207ff7fa5bf5e076b526c2ab9
Thiago <thiago6891@gmail.com>
2026-05-20 15:45:21 -0300

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        const zeroCount = nums.filter((n) => n === 0).length;
+        if (zeroCount > 1) return [...Array(nums.length)].map((_) => 0);
+
+        if (zeroCount === 1) {
+            const result = [...Array(nums.length)].map((_) => 0);
+            result[nums.findIndex((n) => n === 0)] = nums
+                .filter((n) => n !== 0)
+                .reduce((prev, curr) => prev * curr, 1);
+            return result
+        }
+
+        const product = nums.reduce((prev, curr) => prev * curr, 1);
+        return nums.map((n) => product / n);
+    }
+}
