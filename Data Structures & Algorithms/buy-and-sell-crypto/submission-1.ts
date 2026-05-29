1cf722fbfadad7c2aba2b6ff2dbbb47bdf3ff594
Thiago <thiago6891@gmail.com>
2026-05-29 13:39:37 -0300

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let max = 0;
+        let i = 0;
+        let j = 1;
+        while (j < prices.length) {
+            const diff = prices[j] - prices[i];
+            if (diff < 0) {
+                i = j;
+            } else {
+                max = Math.max(max, diff);
+            }
+            j++;
+        }
+        return max;
+    }
+}
